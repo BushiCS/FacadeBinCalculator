b89f833c550ca502f25648976d865141a428d8fc
Yuriy Sviridov <crip.shooter@gmail.com>
2022-10-21 02:09:32 +0300

    Update Main & add BinOps

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        BinOps binOps = new BinOps();
+        System.out.println(binOps.sum("101","1101"));
+        System.out.println(binOps.mult("1001101","1110"));
+    }
+}
