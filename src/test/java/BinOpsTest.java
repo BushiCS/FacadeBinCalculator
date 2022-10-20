495a0a3ca24409e1ac1934e8173667a729c362be
Yuriy Sviridov <crip.shooter@gmail.com>
2022-10-21 02:10:08 +0300

    add tests

--- src/test/java/BinOpsTest.java
+++ src/test/java/BinOpsTest.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class BinOpsTest {
+import org.example.BinOps;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BinOpsTest {
+    private final BinOps binOps = new BinOps();
+
+    @Test
+    @DisplayName("Тест бинарного сложения")
+    void binSumTest() {
+        Assertions.assertEquals("10010", binOps.sum("101", "1101"));
+    }
+
+    @Test
+    @DisplayName("Тест бинарного умножения")
+    void binMultTest() {
+        Assertions.assertEquals("10110", binOps.mult("10", "1011"));
+    }
 }
